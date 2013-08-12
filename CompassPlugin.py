35fbd81dfcf2899cfe4e13ff261fc6c4619a6aee
Grafikart <contact@grafikart.fr>
2013-08-12 20:14:36 +0200

    Updated compass plugin
    The file to refresh is now guessed from the result of the compile

--- CompassPlugin.py
+++ CompassPlugin.py
@@ -8,6 +8,8 @@ import sys
 import sublime
 import sublime_plugin
 import shlex
+import re
+import json
 
 # fix for import order
 
@@ -44,14 +46,15 @@ class CompassThread(threading.Thread):
             return {}
 
     def __init__(self, dirname, on_compile):
+        ##TODO: Proper handler for this
         try:
-            self.dirname = self.getLocalOverride().get('dirname') \
+            self.dirname = self.getLocalOverride.get('dirname') \
             or dirname.replace('\\', '/')
         except Exception as e:
             self.dirname = dirname.replace('\\', '/')
 
         try:
-            self.command = self.getLocalOverride().get('command') or 'compass compile'
+            self.command = self.getLocalOverride.get('command') or 'compass compile'
         except Exception as e:
             self.command = 'compass compile'
 
@@ -60,24 +63,55 @@ class CompassThread(threading.Thread):
         self.on_compile = on_compile
         threading.Thread.__init__(self)
 
+    #  Check if a config.rb file exists in the current directory
+    #  If no config.rb is found then check the parent and stop when root directory is reached
     def check_for_compass_config(self):
-        return os.path.isfile(os.path.join(self.dirname, "config.rb"))
+        if os.path.isfile(os.path.join(self.dirname, "config.rb")):
+            return True
+        dirname = os.path.abspath(os.path.join(self.dirname, os.pardir)).replace('\\', '/')
+        if self.dirname == dirname:
+            return False
+        else:
+            self.dirname = dirname
+            return self.check_for_compass_config()
+
+    # Generate config.rb file
+    def generate_conf_rb(self, dirname):
+        config_rb = """http_path = "/"
+        css_dir = "."
+        sass_dir = "."
+        images_dir = "img"
+        javascripts_dir = "javascripts"
+        output_style = :nested
+        relative_assets=true
+        line_comments = false
+        """
+        with open(os.path.join(dirname, "config.rb"), 'w') as f:
+            f.write(config_rb)
+        self.dirname = dirname
+        return
 
     def run(self):
+        dirname = self.dirname
         if not self.check_for_compass_config():
-            self.dirname = os.path.abspath(os.path.join(self.dirname, os.pardir))
-            if not self.check_for_compass_config():
+            if json.load(open(os.path.join(sublime.packages_path(),'LiveReload','CompassPlugin.sublime-settings')))["create_configrb"]:
+                self.generate_conf_rb(dirname)
+            else:
                 sublime.error_message("Could not find Compass config.rb. Please check your sublime-project file and adjust settings accordingly!")
                 return
         cmd = shlex.split(self.command)
         cmd.append(self.dirname)
-        p = subprocess.Popen(cmd, shell=False, stdin=subprocess.PIPE,
+        p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT)
         compiled = p.stdout.read()
+
+        # Find the file to refresh from the console output
         if compiled:
-            print(compiled)
-            self.on_compile()
+            matches = re.findall('\S+\.css', compiled.decode("utf-8"))
+            if len(matches) > 0:
+                for match in matches:
+                    self.on_compile(match)
 
 
 class CompassPreprocessor(LiveReload.Plugin,
@@ -92,15 +126,12 @@ class CompassPreprocessor(LiveReload.Plugin,
     def on_post_save(self, view):
         self.original_filename = os.path.basename(view.file_name())
         if self.should_run(self.original_filename):
-            self.file_name_to_refresh = \
-                self.original_filename.replace('.scss', '.css'
-                    ).replace('.sass', '.css')
             dirname = os.path.dirname(view.file_name())
             CompassThread(dirname, self.on_compile).start()
 
-    def on_compile(self):
+    def on_compile(self, file_to_refresh):
         settings = {
-            'path': self.file_name_to_refresh,
+            'path': file_to_refresh,
             'apply_js_live': False,
             'apply_css_live': True,
             'apply_images_live': True,
