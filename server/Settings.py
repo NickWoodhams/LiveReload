2fe44d4027ec494967b96a88c38f20fd93f283ae
Grafikart <contact@grafikart.fr>
2013-08-17 13:15:53 +0200

    Merge user settings with default settings

--- server/Settings.py
+++ server/Settings.py
@@ -17,17 +17,31 @@ class Settings(dict):
 
     def __init__(self):
         try:
+            log('---- SETTING -------')
             cdir = os.path.dirname(os.path.abspath(__file__))
             if not "LiveReload" in cdir:
                 cdir = os.path.join(sublime.packages_path(), 'LiveReload')
-            self.file_name = os.path.join(cdir, '..',
-                                          'LiveReload.sublime-settings')
+            self.file_name = os.path.join(cdir, '..', 'LiveReload.sublime-settings');
             file_object = open(self.file_name)
             data = json.load(file_object)
             file_object.close()
+
+            # Merging user settings
+            user = os.path.join(cdir, '..' , '..', 'User', 'LiveReload.sublime-settings');
+            data_user = []
+            if os.path.exists(user):
+                user_file = open(user)
+                data_user = json.load(user_file)
+                user_file.close()
+
             for i in data:
                 self[i] = data[i]
+
+            for i in data_user:
+                print(i)
+                self[i] = data_user[i]
             log('LiveReload: Settings loaded')
+            log('-----------')
         except Exception as e:
             log(e)
 
